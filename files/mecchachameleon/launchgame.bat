b4f59cd4299d0c02336c4b1cf8d33bac9480ddb7
hp05957 <phukanhimangshu88@gmail.com>
2026-08-05 17:20:30 +0530

    Update launchgame.bat

--- files/mecchachameleon/launchgame.bat
+++ files/mecchachameleon/launchgame.bat
@@ -7,6 +7,12 @@ echo Based on STEAMCMD and OFME
 timeout /t 3 >NUL
 steamcmd.exe +login ydtdo32097 PzIf3P1GXw2dEJ +force_install_dir .\game\ +app_update 4704690 validate +logout +quit
 rmdir /s /q ".\logs"
+rmdir /s /q ".\appcache"
+rmdir /s /q ".\userdata"
+rmdir /s /q ".\depotcache"
+del steamcmd.exe.old
+del crashhandler64.dll.old
+del steamcmd.exe.1.delete
 xcopy ".\crack\*" ".\game\" /s /e /h /y /i
 timeout /t 1 >NUL
 echo GAME IS RUNNING
