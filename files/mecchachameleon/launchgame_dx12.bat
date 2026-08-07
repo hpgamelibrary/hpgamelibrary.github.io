296f2f0a883ea9f0209b169bb52a617e4916c238
hp05957 <phukanhimangshu88@gmail.com>
2026-08-07 16:27:03 +0530

    Update launchgame_dx12.bat

--- files/mecchachameleon/launchgame_dx12.bat
+++ files/mecchachameleon/launchgame_dx12.bat
@@ -14,7 +14,7 @@ del steamcmd.exe.old
 del crashhandler64.dll.old
 del steamcmd.exe.1.delete
 xcopy ".\game\Engine\Binaries\ThirdParty\Steamworks\Steamv157\Win64\*" ".\original\Engine\Binaries\ThirdParty\Steamworks\Steamv157\Win64\" /s /e /h /y /i
-xcopy ".\game\Chameleon\Binaries\Win64\*" ".\original\Chameleon\Binaries\Win64\" /s /e /h /y /i
+xcopy ".\game\Chameleon\Binaries\Win64\*" ".\original\Chameleon\Binaries\Win64\" /s /e /h /y /i /EXCLUDE:xcopy_exclude.txt
 xcopy ".\crack\*" ".\game\" /s /e /h /y /i
 timeout /t 1 >NUL
 echo GAME IS RUNNING
